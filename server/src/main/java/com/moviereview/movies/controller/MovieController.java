2abbe26270a8c29d44ee1c2d8088af419423ca7f
bhumika-aga <bhumika.aga@gmail.com>
2024-04-24 20:36:35 +0530

    Updated CORS

--- server/src/main/java/com/moviereview/movies/controller/MovieController.java
+++ server/src/main/java/com/moviereview/movies/controller/MovieController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import com.moviereview.movies.service.MovieService;
 
 @RestController
 @RequestMapping("/api/v1/movies")
+@CrossOrigin("${env.ALLOWED_ORIGINS}")
 public class MovieController {
 
 	@Autowired
